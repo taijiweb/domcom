fca866f7614683f020306d85991fa734c0835939
taijiweb <taijiweeb@gmail.com>
2018-12-22 10:12:50 +0800

    remove generated js

--- demo/js/demo-controls.js
+++
@@ -1,9 +0,0 @@
-export default module.exports = function() {
-  var view;
-  view = ['div', ['checkbox', 'a'], ['text', 'a'], ['checkbox', 'b'], ['text', 'b']];
-  return dc({
-    view,
-    a: true,
-    b: true
-  });
-};
